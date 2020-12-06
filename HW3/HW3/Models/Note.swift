f11b2f04accbdf095fe1766ec975c184f43cadd0
sluzhynskyi <sluzhynskyi@gmail.com>
2020-12-06 14:44:41 +0200

    init coredata

--- HW3/HW3/Models/Note.swift
+++ HW3/HW3/Models/Note.swift
@@ -1,104 +1,22 @@
 import Foundation
 
-struct Note {
+class Note {
     let noteId: Int
-    var name, text: String
+    var name = "", text: String = ""
     var tags: Set <String> = Set<String>()
     var isFavorite: Bool = false
     var creationDate = Date()
     var deletionDate: Date?
-
+    init(noteId: Int, name: String, text: String, tags: Set<String>) {
+        self.noteId = noteId
+        self.name = name
+        self.text = text
+        self.tags = tags
+    }
 }
 
 extension Note: Equatable {
     static func == (lhs: Note, rhs: Note) -> Bool {
-        return lhs.name == rhs.name && lhs.text == lhs.text // Comment: id?
+        return lhs.noteId == rhs.noteId
     }
-}
-
-class NoteDataManager {
-    var noteId = 0
-    var dataSource: [Note] = []
-    var removedSource: [Note] = []
-    var filteredNotes: [Note] = []
-
-    // CRUD functions
-
-    func createNote(name noteName: String, text noteText: String, tags noteTags: Set <String> = []) -> Note {
-        let note = Note(noteId: self.noteId, name: noteName, text: noteText, tags: noteTags)
-        self.dataSource.append(note)
-        self.noteId += 1
-        return note
-    }
-    func getNote(id noteId: Int) -> Note? {
-        dataSource.first(where: { $0.noteId == noteId })
-    }
-
-    func setNote(id noteId: Int, note: Note) -> Bool {
-        for (index, element) in self.dataSource.enumerated() {
-            if element.noteId == noteId {
-                self.dataSource[index] = note
-                return true
-            }
-        }
-        return false
-    }
-
-    func popNote(id noteId: Int) -> Note? {
-        for (index, element) in self.dataSource.enumerated() {
-            if element.noteId == noteId {
-                self.dataSource[index].deletionDate = Date()
-                self.removedSource.append(self.dataSource[index])
-                self.dataSource.remove(at: index)
-                return element
-            }
-        }
-        return nil
-    }
-    // Additional functions
-    func toggleFavorite(id noteId: Int) -> Bool {
-        // makes note favorite propery inverse to previous
-        for (index, element) in self.dataSource.enumerated() {
-            if element.noteId == noteId {
-                let note = self.dataSource[index]
-                self.dataSource[index].isFavorite = !note.isFavorite
-                return true
-            }
-        }
-        return false
-    }
-
-    func restoreNote(id noteId: Int) -> Note? {
-        for (index, element) in self.removedSource.enumerated() {
-            if element.noteId == noteId {
-                self.dataSource.append(element)
-                self.removedSource.remove(at: index)
-                return element
-            }
-        }
-        return nil
-    }
-
-    func searchBy(name noteName: String) {
-        let filtered = self.dataSource.filter {
-            $0.name.lowercased().contains(noteName.lowercased())
-        }
-        self.filteredNotes = filtered
-    }
-
-    func alreadyPresented(note: Note) -> Bool {
-        dataSource.contains(note)
-    }
-
-    func filterBy(tags: Set <String>) {
-        let filtered = dataSource.filter {
-            $0.tags.intersection(tags).count == tags.count
-        }
-        self.filteredNotes = filtered
-
-    }
-    func sortingNotes() {
-        self.dataSource.sort { $0.name < $1.name }
-    }
-
 }
